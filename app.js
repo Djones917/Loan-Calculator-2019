c8a56e27cf50924256e061f1e8f4c4ba3129ab3e
Derek <nightwing917@hotmail.com>
2019-01-25 19:11:44 -0700

    Setting up calculate function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Connected!');
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculating Results
+function calculateResults(e) {
+    console.log('Calculating...');
+
+
+    e.preventDefault();
+}
