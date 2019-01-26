67ee5d09801e146820d05576f893486eac5cac17
Derek <nightwing917@hotmail.com>
2019-01-25 19:22:37 -0700

    added the last of the variables to function

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ function calculateResults(e) {
     // UI Variables
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totolInterest = document.getElementById('total-interest');
 
 
     e.preventDefault();
